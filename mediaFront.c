6c40a1edc2a3b61a7ecb44e8f1b60927ded8c62f
saulex16 <scastaneda@itba.edu.ar>
2021-07-09 04:41:25 -0300

    Querys 1 y 2 :  Agregado de encabezados

--- mediaFront.c
+++ mediaFront.c
@@ -97,13 +97,15 @@ TContent createContent(char * line, const char * delim )
 void query1(mediaADT media, char * filePath){
     FILE * file=fopen(filePath, "w");
 
+    fprintf(file,"%s;%s;%s\n" , "year" ,"films", "series");
+
     toBeginYear(media);
-    fprintf(file, "year; films; series\n");
+    fprintf(file, "year;genre;films\n");
     while (hasNextYear(media)){
         unsigned short year= nextYear(media);
         size_t MYears= countContentByYear(media, year, CONTENTTYPE_MOVIE);
         size_t SYears= countContentByYear(media, year, CONTENTTYPE_SERIES);
-        fprintf(file, "%u; %zu; %zu\n", year, MYears, SYears);
+        fprintf(file, "%u;%zu;%zu\n", year, MYears, SYears);
     }
     fclose(file);
 }
@@ -112,18 +114,21 @@ void query2 ( mediaADT media , char * filePath )
 {
     FILE * file = fopen(filePath,"w");
 
+    ///Se agrega el header correspondiende al archivo
+    fprintf(file,"%s;%s;%s\n" , "year" ,"genre", "films");
+
     toBeginYear(media);
-    //Se itera por años validos para obtener los generos y cantidad peliculas por genero
+    ///Se itera por años validos para obtener los generos y cantidad peliculas por genero
     while (hasNextYear(media))
     {
         unsigned int year = nextYear(media);
         toBeginGenre(media,year);
-        //Se itera por generos validos para obtener la cantidad de peliculas del genero actual
+        ///Se itera por generos validos para obtener la cantidad de peliculas del genero actual
         while(hasNextGenre(media))
         {
             char * genre = nextGenre(media);
             size_t countOfMovies = countContentByGenre(media,year,genre,CONTENTTYPE_MOVIE);
-            /*Se tiene año , genero y cantidad de peliculas para el par (año,genero). Se guarda la información en el
+            /**Se tiene año , genero y cantidad de peliculas para el par (año,genero). Se guarda la información en el
             *archivo y se continua la iteracion
              */
             fprintf(file,"%d;%s;%zu\n",year , genre , countOfMovies);
