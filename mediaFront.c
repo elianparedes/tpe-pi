5862093b6ef7d1f6b84607e0568a2d7712ad079e
saulex16 <scastaneda@itba.edu.ar>
2021-07-09 02:04:15 -0300

    Query 2

--- mediaFront.c
+++ mediaFront.c
@@ -1,31 +1,10 @@
 #include "mediaADT.h"
 #include <stdio.h>
 #include <string.h>
-
+#define MAX_GENRES 15
 #define GENRES_COUNT 0
 
 int getDataFromFile(char * filePath);
-
-TContent createContent(char * line, char * delim )
-{
-    TContent newContent ;
-    newContent.titleType = strtok(line,delim);
-    newContent.primaryTitle  = strtok(NULL,delim);
-    newContent.startYear = atoi(strtok(NULL,delim));
-    newContent.endYear = atoi(strtok(NULL,delim));
-
-    char * genres = strtok(NULL,delim);
-
-    newContent.averageRating = atof(strtok(NULL,delim));
-    newContent.numVotes = atoi(strtok(NULL,delim));
-    newContent.runtimeMinutes = atoi(strtok(NULL,delim));
-
-    newContent.genres = malloc(MAX*sizeof(char*));
-    createGenresVec(newContent.genres, genres);
-
-    return newContent;
-
-}
 
 char ** createGenresVec(char ** vec, char * string){
     char * token;
@@ -43,6 +22,54 @@ char ** createGenresVec(char ** vec, char * string){
     vec[i]=NULL;
     return vec;
 }
+
+TContent createContent(char * line, char * delim )
+{
+    TContent newContent ;
+    newContent.titleType = strtok(line,delim);
+    newContent.primaryTitle  = strtok(NULL,delim);
+    newContent.startYear = atoi(strtok(NULL,delim));
+    newContent.endYear = atoi(strtok(NULL,delim));
+    char * tokenAux = strtok(NULL,delim);
+    newContent.genres = malloc(MAX_GENRES*sizeof(char*));
+    createGenresVec(newContent.genres, tokenAux);
+
+    newContent.averageRating = atof(strtok(tokenAux,delim));
+    newContent.numVotes = atoi(strtok(NULL,delim));
+    newContent.runtimeMinutes = atoi(strtok(NULL,delim));
+
+
+
+    return newContent;
+
+}
+
+void query2 ( mediaADT media , char * filePath )
+{
+    FILE * file = fopen(filePath,"w");
+
+    toBeginYear(media);
+    //Se itera por años validos para obtener los generos y cantidad peliculas por genero
+    while (hasNextYear(media))
+    {
+        unsigned int year = nextYear(media);
+        toBeginGenre(media,year);
+        //Se itera por generos validos para obtener la cantidad de peliculas del genero actual
+        while(hasNextGenre(media))
+        {
+            char * genre = nextGenre(media);
+            size_t countOfMovies = countContentByGenre(media,year,genre,CONTENTTYPE_MOVIE);
+            /*Se tiene año , genero y cantidad de peliculas para el par (año,genero). Se guarda la iformacion en el
+            *archivo y se continua la iteracion
+             */
+            fprintf(file,"%d;%s;%llu",year , genre , countOfMovies);
+        }
+    }
+
+    fclose(file);
+}
+
+
 
 int
 main(int argc, char *argv[]) {
