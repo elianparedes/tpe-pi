8ce69f38e844e15b2d10a2dcbaea3cc54419b378
Juan-Burda <jburda@itba.edu.ar>
2021-07-08 23:02:47 -0300

    Función createGenresVec

--- mediaFront.c
+++ mediaFront.c
@@ -1,11 +1,29 @@
 #include "mediaADT.h"
 #include <stdio.h>
+#include <string.h>
 
 #define GENRES_COUNT 0
 
 int getDataFromFile(char * filePath);
 
 TContent createContent(char * line, char * delim);
+
+char ** createGenresVec(char ** vec, char * string){
+    char * token;
+    token = strtok(string, ",");
+    unsigned int i=0;
+    if (strcmp(token, "\\N")==0){
+        vec[i++]= "Género no identificado";
+    }
+    else {
+        while (token != NULL) {
+            vec[i++] = token;
+            token = strtok(NULL, ",");
+        }
+    }
+    vec[i]=NULL;
+    return vec;
+}
 
 int
 main(int argc, char *argv[]) {
