f5f84a8167d08d94ccab0bd4bda95b11223e5b2b
Elian Paredes <eparedes@itba.edu.ar>
2021-07-09 04:56:32 -0300

    Implementación de la funcion query3

--- mediaFront.c
+++ mediaFront.c
@@ -1,21 +1,35 @@
 #include "mediaADT.h"
 #include <stdio.h>
 #include <string.h>
+
+#define MIN_YEAR 1850
 #define MAX_GENRES 15
 #define BUFFER_SIZE 128
 
 int getDataFromFile(mediaADT media, const char * filePath);
+
 char ** createGenresVec(char ** vec, char * string);
+
 TContent createContent(char * line, const char * delim );
+
 void query1(mediaADT media, char * filePath);
-void query2 ( mediaADT media , char * filePath );
+
+void query2(mediaADT media, char * filePath);
+
+/**
+ * @brief Consulta las peliculas y series más votadas de cada año. Crea un archivo en el directorio especificado
+ * y escribe el mismo con la información obtenida.
+ *
+ * @param media ADT creado para el manejo de películas/series.
+ * @param filePath Directorio destino del archivo.
+ */
+void query3(mediaADT media, char * filePath);
 
 int main(int argc, char *argv[]) {
     return 0;
 }
 
 int getDataFromFile(mediaADT media, const char * filePath){
-
     char buffer[BUFFER_SIZE];
 
     FILE *file = fopen(filePath, "r");
@@ -52,17 +66,22 @@ TContent createContent(char * line, const char * delim )
 {
     TContent newContent ;
     newContent.titleType = strtok(line,delim);
-    newContent.primaryTitle  = strtok(NULL,delim);
+
+    char * title = strtok(NULL,delim);
+    newContent.primaryTitle = malloc(strlen(title) + 1);
+    strcpy(newContent.primaryTitle, title);
+
     newContent.startYear = atoi(strtok(NULL,delim));
     newContent.endYear = atoi(strtok(NULL,delim));
 
     char * tokenAux = strtok(NULL,delim);
-    newContent.genres = malloc(MAX_GENRES*sizeof(char*));
-    createGenresVec(newContent.genres, tokenAux);
 
-    newContent.averageRating = atof(strtok(tokenAux,delim));
+    newContent.averageRating = atof(strtok(NULL,delim));
     newContent.numVotes = atoi(strtok(NULL,delim));
     newContent.runtimeMinutes = atoi(strtok(NULL,delim));
+
+    newContent.genres = malloc(MAX_GENRES*sizeof(char*));
+    createGenresVec(newContent.genres, tokenAux);
 
     return newContent;
 }
@@ -102,5 +121,32 @@ void query2 ( mediaADT media , char * filePath )
         }
     }
 
+    fclose(file);
+}
+
+void query3(mediaADT media, char * filePath){
+    ///Se crea el archivo, se abre en modo "write" para escribir sobre el mismo.
+    FILE * file = fopen(filePath, "w");
+
+    ///En primer lugar, se añade el header al archivo.
+    fprintf(file,"startYear;film;votesFilm;ratingFilm;serie;votesSerie;ratingSerie\n");
+
+    ///Se inicia el iterador por años.
+    toBeginYear(media);
+    while (hasNextYear(media)){ ///< Mientras aún existan años validos, se seguirá escribiendo el archivo.
+        unsigned short year = nextYear(media);
+
+        ///Se obtiene la película y la serie más votada del año correspondiente.
+        TContent movie = mostVoted(media, year, CONTENTTYPE_MOVIE);
+        TContent series = mostVoted(media, year, CONTENTTYPE_SERIES);
+
+        ///Se imprime en el archivo la información con el formato correspondiente.
+        fprintf(file, "%d;%s;%.1f;%lu;%s;%.1f;%lu\n",year,
+                movie.primaryTitle,movie.averageRating,movie.numVotes,
+                series.primaryTitle,series.averageRating,series.numVotes);
+
+    }
+
+    ///Se finaliza la escritura del archivo.
     fclose(file);
 }
